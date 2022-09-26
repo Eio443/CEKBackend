99afb36047f26164c0e50410635716cd47e88408
Chris <lifeanddigit@gmail.com>
2022-09-26 22:13:42 +0100

    Migration Error

--- Domain/Course.cs
+++ Domain/Course.cs
@@ -5,10 +5,10 @@ namespace Domain
     public class Course
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public DateTime Date { get; set; }
-        public string Creator { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
+        public string? Creator { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
     }
 }
